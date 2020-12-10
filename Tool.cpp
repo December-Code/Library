5377e041192ce69c781ae713f05969edc748155b
YU-RU <leo32095@gmail.com>
2020-12-10 17:00:45 +0800

    this day c++ complete

--- Tool.cpp
+++ Tool.cpp
@@ -12,13 +12,36 @@ int main()
     cin >> countS;
     cout << "Input which number End?" << endl;
     cin >> countE;
+    int count_add = 0;
 
     file.open("Web.txt", ios::out);
     if (file.fail())
         cout << "Can't open the file";
     else
     {
-        int count_add = 0;
+        // for (int i = countE; i > countS - 1; i--)
+        // {
+        //     if (count_add % 5 == 0)
+        //     {
+        //         file << "<li><a href=\"#\">" + to_string(i) + "~" + to_string(i - 4) + "期</a>" << endl;
+        //         file << "<ul>" << endl;
+        //     }
+        //     file << "<li><a href=";
+        //     file << "\"https://library-r.ntust.edu.tw/var/file/49/1049/img/2781/Newsletter";
+        //     file << setw(3) << setfill('0') << to_string(i);
+        //     file << ".pdf\"" << endl;
+        //     file << "target=\"_blank\" title=\"";
+        //     file << setw(3) << setfill('0') << to_string(i);
+        //     file << "\">";
+        //     file << setw(3) << setfill('0') << to_string(i);
+        //     file << "期</a></li>" << endl;
+        //     if (count_add % 5 == 4 or i == countS)
+        //     {
+        //         file << "</ul>" << endl;
+        //         file << "</li>" << endl;
+        //     }
+        //     count_add += 1;
+        // }
         for (int i = countE; i > countS - 1; i--)
         {
             if (count_add % 4 == 0)
