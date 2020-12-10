b292735d0f876282eaec6fa5154b41df0521e0e8
YU-RU <leo32095@gmail.com>
2020-12-10 15:06:59 +0800

    this day c++ update

--- Tool.cpp
+++ Tool.cpp
@@ -1,24 +1,44 @@
 #include <iostream>
 #include <fstream>
+#include <iomanip>
 
 using namespace std;
 int main()
 {
     fstream file;
-    int count = 0;
-    // string data =fmt::;
+    int count = 1; //從1開始
+    cout << "input which number start?" << endl;
+    cin >> count;
+
     file.open("Web.txt", ios::out);
     if (file.fail())
         cout << "Can't open the file";
     else
     {
-        for (int i = 100; i > count; i--)
+        int count_add = 0;
+        for (int i = count + 99; i > count - 1; i--)
         {
+            if (count_add % 4 == 0)
+            {
+                file << "<tr>" << endl;
+            }
             file << "<td><a href=";
-            file << "\"https://library-r.ntust.edu.tw/var/file/49/1049/img/2763/chiall10905.pdf\"" << endl;
-            file << "target=\"_blank\" title=\"0" + to_string(i) + "\">0" + to_string(i) + "期</a></td>" << endl;
+            file << "\"https://library-r.ntust.edu.tw/var/file/49/1049/img/2781/Newsletter";
+            file << setw(3) << setfill('0') << to_string(i);
+            file << ".pdf\"" << endl;
+            file << "target=\"_blank\" title=\"";
+            file << setw(3) << setfill('0') << to_string(i);
+            file << "\">";
+            file << setw(3) << setfill('0') << to_string(i);
+            file << "期</a></td>" << endl;
+            if (count_add % 4 == 3)
+            {
+                file << "</tr>" << endl;
+            }
+            count_add += 1;
         }
         file.close();
+        cout << "Success" << endl;
     }
 
     return 0;
